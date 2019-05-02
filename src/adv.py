69578088dbcb997663d660a05ec87e7fc18e847b
m-j-hacker <mjhacker@gmail.com>
2019-05-02 12:19:51 -0500

    Add working REPL prompt to adv.py

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,8 @@
 from room import Room
 from player import Player
+from colorama import init
+init()
+
 # Declare all the rooms
 
 room = {
@@ -36,6 +39,27 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+def print_color(message):
+
+
+def get_room(cmd, current_room):
+    # This function uses if statements
+    if cmd == 'n':
+        return current_room.n_to
+    elif cmd == 's':
+        return current_room.s_to
+    elif cmd == 'w':
+        return current_room.w_to
+    elif cmd == 'e':
+        return current_room.e_to
+    else:
+        return "Error: improper input (must be n, s, e, or w)"
+
+def fetch_room(cmd, current_room):
+    # This function uses concatenated strings
+    moving = cmd + "_to"
+    return getattr(current_room, moving)
+
 acceptable_values = ('n','s','e','w','q')
 # Make a new player object that is currently in the 'outside' room.
 warrior = Player("Hacker", room["outside"])
@@ -51,18 +75,27 @@ warrior = Player("Hacker", room["outside"])
 # If the user enters "q", quit the game.
 
 while True:
-    current_room = warrior.room
-    
-    print(current_room.name)
-    print(current_room.description)
-    prompt = input("Which way do we go? (type n e s w, q to quit) HP=100 MP=100 $> ")
+    print("\n" + warrior.room.name)
+    print("\n" + warrior.room.description)
+    prompt = input("\n\nWhich way do we go?\n (n e s w to move, q to quit) \nHP=100 MP=100 $> ")
     if prompt not in acceptable_values:
         print("Please type a valid command (n, e, s, w, or q)")
     elif prompt == 'q':
         print("Thank you for playing Lambda Quest!")
         break
     else:
-        prompt = prompt + '_to'
-        for key, value in room.items():
+        new_room = get_room(prompt, warrior.room)
+        if new_room != None:
+            warrior.change_room(new_room)
+        else:
+            print("\nYou can't go that way!!\n")
+        # for key, value in room:
+        #     if key.find(str(prompt + "_to")) and value != type(None):
+        #         print("STARTED FROM THE BOTTOM NOW WE HEEEERE")
             
-                # warrior.change_room(value)
+        # directions = current_room.__dict__.keys() 
+        # for key in directions:
+        #     if key.find("_to") > 0:
+        #         global direction_to_go
+        #         direction_to_go = str(prompt + "_to")
+        # warrior.change_room(room[])
