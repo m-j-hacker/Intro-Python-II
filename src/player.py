b597e31a4fdfb2d35fa9b303abdfdfab4d921d38
m-j-hacker <mjhacker@gmail.com>
2019-05-02 15:56:29 -0500

    Update player and room classes w/ parents

--- src/player.py
+++ src/player.py
@@ -1,9 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from lib import NameStorage
 
-class Player:
-    def __init__(self, name, room):
-        self.name = name
+class Player(NameStorage):
+    def __init__(self, name, room, storage=[]):
+        super().__init__(name, storage=storage)
         self.room = room
     
     def change_room(self, room):
--- src/room.py
+++ src/room.py
@@ -1,10 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+from lib import Description
 
-class Room:
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
+class Room(Description):
+    def __init__(self, name, description, storage=[]):
+        super().__init__(name, description, storage=storage)
         self.n_to = None
         self.s_to = None
         self.e_to = None
